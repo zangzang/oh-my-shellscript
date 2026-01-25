d5136fdc62df025c8430c9f9b7f916917b5bc992
Jang Jongwoo <aboy@outlook.com>
2026-01-25 21:19:17 +0900

    chore: Clean up build artifacts and add .gitignore

--- linux-setup/test/dev.rust/hello/target/debug/deps/hello-ff99748284b39665.d
+++
@@ -1,5 +0,0 @@
-/mnt/d/ws/zangzang/my-shell-script/linux-setup/test/dev.rust/hello/target/debug/deps/hello-ff99748284b39665.d: src/main.rs
-
-/mnt/d/ws/zangzang/my-shell-script/linux-setup/test/dev.rust/hello/target/debug/deps/hello-ff99748284b39665: src/main.rs
-
-src/main.rs:
--- linux-setup/test/dev.rust/hello/target/debug/hello.d
+++
@@ -1,1 +0,0 @@
-/mnt/d/ws/zangzang/my-shell-script/linux-setup/test/dev.rust/hello/target/debug/hello: /mnt/d/ws/zangzang/my-shell-script/linux-setup/test/dev.rust/hello/src/main.rs
